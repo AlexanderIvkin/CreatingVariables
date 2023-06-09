a9c2748a232e5a734c3992d80933327d10c79271
AlexanderIvkin <dvakota@bk.ru>
2023-06-09 13:15:10 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int variablesCount = 10;
+            bool existingBasicTypes = true;
+            string nameLesson = ("Практика в создании переменных.");
+            float spentTime = 1.5f;
+            char desiredRating = 'A';
+            uint desiredRatingNumber = 5;
+            ulong maxAttemptsNumber = ulong.MaxValue;
+            byte thisAttemptNumber = 1;
+            double piNumber = 3.141592653589793;
+            sbyte weatherOusideWindow = 21;
+        }
+    }
+}
